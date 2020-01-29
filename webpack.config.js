bf02bd8e29cbbd0308fbcf0953dac8ee0b7ef0ca
mrosoff <mrosoff@ramscolostate.edu>
2020-01-29 14:27:42 -0700

    Install Clean Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     watch: false,
@@ -23,6 +24,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         })
