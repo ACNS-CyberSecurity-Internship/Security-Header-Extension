789b4b32cdeb74837ace7eb473d80fadb77b3c08
tanner <tannerjm@rams.colostate.edu>
2020-02-18 00:23:02 -0700

    Maybe fixed local storage test again

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ let mode = 0;
 if(window.localStorage) {
 	// localStorage can be used
 	mode = localStorage.getItem("modeVal");
-} else {
-	// can't be used
 }
 //showRec = localStorage.getItem("showRec");
 
@@ -478,8 +476,6 @@ export function darkMode() {
 	}
 	if(window.localStorage) {
 		localStorage.setItem("modeVal", mode);
-	} else {
-		throw error
 	}
 	location.reload();
 }
@@ -566,27 +562,3 @@ export function setDisplay(items, displayType)
 		items[i].style.display = displayType;
 	}
 }
-
-function setCookie(cname, cvalue, exdays) {
-	let d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	let expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i <ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
-}
-
